ef992362f547e4a5b688b521bab3de4f5b2beb1e
kv3rk <kverk07@gmail.com>
2026-02-25 02:25:23 +0300

    Made outstanding output with thymeleaf for both pages

--- src/main/java/ru/translator/eng_rus/Controller/TranslateController/TranslateController.java
+++ src/main/java/ru/translator/eng_rus/Controller/TranslateController/TranslateController.java
@@ -4,7 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.translator.eng_rus.POJO.WrongStringPOJO;
 import ru.translator.eng_rus.Service.TranslateService.TranslateService;
 
 @Slf4j
@@ -17,11 +18,22 @@ public class TranslateController {
         this.translateService = translateService;
     }
 
-    @GetMapping("/{str}")
-    public String get(@PathVariable String str, Model model) {
-        log.info("Got value {}", str);
-        model.addAttribute("wrongString", str);
-        model.addAttribute("rightString", translateService.get(str));
+    @GetMapping("/")
+    public String showPage(Model model) {
+        if (!model.containsAttribute("pojo")) {
+            model.addAttribute("pojo", new WrongStringPOJO());
+        }
+        log.info("Initialized absent POJO object");
         return "translate-page";
+    }
+
+    @PostMapping("/convert")
+    public String save(@ModelAttribute("pojo") WrongStringPOJO pojo,
+                       RedirectAttributes redirectAttributes) {
+        log.info("Got necessary wrong string {}", pojo.getWrongString());
+        String result = translateService.get(pojo.getWrongString());
+        pojo.setRightString(result);
+        redirectAttributes.addFlashAttribute("pojo", pojo);
+        return "redirect:/translate/";
     }
 }
